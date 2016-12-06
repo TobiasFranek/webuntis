ccb24b0b919b10c702bdd4f061b7bd643fb257c5
Tobias Franek <tobias.franek@gmail.com>
2016-12-06 18:41:34 +0100

    added Exams model

---
+++ src/Models/Exams.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: Tobias Franek <tobias.franek@gmail.com>
+ * Project: webuntis
+ * Date: 06.12.16
+ * Time: 18:40
+ */
+
+namespace Webuntis\Models;
+
+
+class Exams extends AbstractModel {
+    public function parse($data) {
+        // TODO: Implement parse() method.
+    }
+    public function serialize() {
+        // TODO: Implement serialize() method.
+    }
+}
