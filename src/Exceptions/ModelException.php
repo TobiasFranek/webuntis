113ec811f4f8842e0ebc7b9e017ee6797677880a
Tobias Franek <tobias.franek@gmail.com>
2016-12-08 15:37:38 +0100

    added ModelException

---
+++ src/Exceptions/ModelException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: Tobias Franek <tobias.franek@gmail.com>
+ * Project: webuntis
+ * Date: 08.12.16
+ * Time: 15:16
+ */
+
+namespace Webuntis\Exceptions;
+
+
+class ModelException extends \DomainException {
+
+}
