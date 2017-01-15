f99d7034d3d62dc1311eb9e58d714fa43103d911
Tobias Franek <tobias.franek@gmail.com>
2017-01-15 19:41:05 +0100

    added docblocks

--- src/Repositories/SubstitutionsRepository.php
+++ src/Repositories/SubstitutionsRepository.php
@@ -1,10 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * Author: Tobias Franek <tobias.franek@gmail.com>
- * Project: webuntis
- * Date: 15.01.17
- * Time: 19:26
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
  */
 
 namespace Webuntis\Repositories;
@@ -14,6 +23,11 @@ use Webuntis\Exceptions\RepositoryException;
 use Webuntis\Models\AbstractModel;
 use Webuntis\Util\ExecutionHandler;
 
+/**
+ * Class SubstitutionsRepository
+ * @package Webuntis\Repositories
+ * @author Tobias Franek <tobias.franek@gmail.com>
+ */
 class SubstitutionsRepository extends Repository {
     /**
      * gets the period object from the current day or the given date and can be filtered
