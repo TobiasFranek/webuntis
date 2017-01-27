217e1e8d258a94741dc457dae51ee1f0db2475c2
Tobias Franek <tobias.franek@gmail.com>
2017-01-27 15:25:23 +0100

    added TypeHandler

---
+++ src/Types/TypeHandler.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace Webuntis\Types;
+
+
+use Webuntis\Configuration\YAMLConfiguration;
+use Webuntis\Models\AbstractModel;
+use Webuntis\Types\Interfaces\TypeInterface;
+
+class TypeHandler {
+
+    /**
+     * @var TypeInterface[]
+     */
+    private static $types = [
+        'string' => StringType::class
+    ];
+
+    public function __construct() {
+        $this->loadCostumTypes();
+    }
+
+    private function loadCostumTypes() {
+        $additionalTypes = YAMLConfiguration::getAdditionalTypes();
+        foreach ($additionalTypes as $key => $value) {
+            self::$types[$key] = $value;
+        }
+    }
+
+    public function handle(AbstractModel &$model, $data, $fields) {
+        foreach($fields as $key => $value) {
+            self::$types[$value['type']]::execute($model, $data, [$key => $value]);
+        }
+    }
+}
