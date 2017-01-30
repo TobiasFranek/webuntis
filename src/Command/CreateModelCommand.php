23b9f060f954e8b7177750ba9e970cb652577e5d
Tobias Franek <tobias.franek@gmail.com>
2017-01-30 20:32:01 +0100

    added the CreateRepositoryCommand after the generation process of the model

--- src/Command/CreateModelCommand.php
+++ src/Command/CreateModelCommand.php
@@ -33,6 +33,7 @@ use Memio\Model\Phpdoc\ReturnTag;
 use Memio\Model\Phpdoc\VariableTag;
 use Memio\Model\Property;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
@@ -207,5 +208,19 @@ class CreateModelCommand extends Command {
 
         $fs->dumpFile($fullPath, $generatedCode);
         $fs->dumpFile($fullPathYML, Yaml::dump($ymlConfig, 6));
+
+        $question = new ConfirmationQuestion('Do you want to create an additional repository class?[Y/N]: ', false);
+        if (!$helper->ask($input, $output, $question)) {
+            return;
+        }
+
+        $command = $this->getApplication()->find('webuntis:generate:repository');
+        $arguments = [
+            'pathToModelYML' => $fullPathYML
+        ];
+
+        $argumentInput = new ArrayInput($arguments);
+
+        $returnCode = $command->run($argumentInput, $output);
     }
 }
