ff07990489e65d6e589de94b101a7293a684b8d1
Tobias Franek <tobias.franek@gmail.com>
2017-01-30 21:19:57 +0100

    fixed path in require_once

--- bin/console.php
+++ bin/console.php
@@ -1,6 +1,11 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
+if(file_exists(__DIR__.'/../../../../vendor/autoload.php')) {
+    require_once __DIR__.'/../../../../vendor/autoload.php';
+} else {
+    require_once __DIR__.'/../vendor/autoload.php';
+
+}
 
 use Symfony\Component\Console\Application;
 use Webuntis\Command\CreateModelCommand;
