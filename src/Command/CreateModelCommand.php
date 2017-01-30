1eb6ce8d0c84303a6e234da5b4a3d25b1f6baea6
Tobias Franek <tobias.franek@gmail.com>
2017-01-30 19:55:58 +0100

    added doc headers

--- src/Command/CreateModelCommand.php
+++ src/Command/CreateModelCommand.php
@@ -1,4 +1,20 @@
 <?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
 
 namespace Webuntis\Command;
 
@@ -27,6 +43,11 @@ use Webuntis\Exceptions\ModelException;
 use Webuntis\Models\AbstractModel;
 use Webuntis\Types\TypeHandler;
 
+/**
+ * Class CreateModelCommand
+ * @package Webuntis\Command
+ * @author Tobias Franek <tobias.franek@gmail.com>
+ */
 class CreateModelCommand extends Command {
     protected function configure() {
         $this->setName('webuntis:generate:model')
