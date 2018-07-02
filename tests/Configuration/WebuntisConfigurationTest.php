2cb5620272f286586cc1704fc26cc342dff81596
Tobias Franek <tobias.franek@gmail.com>
2018-07-02 10:00:22 +0200

    #19 added WebuntisConfigurationTest

---
+++ tests/Configuration/WebuntisConfigurationTest.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+use Webuntis\Configuration\WebuntisConfiguration;
+use Webuntis\Repositories\Repository;
+
+final class WebuntisConfigurationTest extends TestCase
+{
+    public function testSetConfig() : void
+    {
+        WebuntisConfiguration::setConfig([
+            'test' => 'test'
+        ]);
+
+        $this->assertEquals(['test' => 'test'], WebuntisConfiguration::getConfig());
+
+        WebuntisConfiguration::setConfig([
+            'test' => 'test',
+            'disableCache' => 'true'
+        ]);
+
+        $this->assertEquals(true, Repository::$disabledCache);
+    }
+
+    public function testAddConfig() : void
+    {
+        WebuntisConfiguration::setConfig([
+            'test' => 'test'
+        ]);
+
+        $this->assertEquals(['test' => 'test'], WebuntisConfiguration::getConfig());
+        
+        WebuntisConfiguration::addConfig([
+            'test1' => 'test1' 
+        ]);
+
+        $this->assertEquals(['test' => 'test', 'test1' => 'test1'], WebuntisConfiguration::getConfig());
+
+    }
+}
