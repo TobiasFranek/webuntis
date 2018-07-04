b62214503d36de96702b5da485044c05a57c7729
Tobias Franek <tobias.franek@gmail.com>
2018-07-04 14:23:16 +0200

    #19 added SchoolyearsRepositoryTest

---
+++ tests/Repositories/SchoolyearsRepositoryTest.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace Webuntis\Tests\Repositories;
+
+use PHPUnit\Framework\TestCase;
+use Webuntis\Repositories\SchoolyearsRepository;
+use Webuntis\Models\Schoolyears;
+use Webuntis\Handler\ExecutionHandler;
+use Webuntis\Configuration\WebuntisConfiguration;
+
+final class SchoolyearsRepositoryTest extends TestCase
+{
+    public function setUp() : void 
+    {
+        $config = new WebuntisConfiguration([ 
+            'default' => [
+                   //f.e. thalia, cissa etc.
+                    'server' => 'yourserver',
+                    'school' => 'yourschool',
+                    'username' => 'yourusername',
+                    'password' => 'yourpassword'
+                ],
+            'admin' => [
+                   //f.e. thalia, cissa etc.
+                    'server' => 'yourserver',
+                    'school' => 'yourschool',
+                    'username' => 'youradminusername',
+                    'password' => 'youradminpassword'
+            ],
+            'security_manager' => 'Webuntis\Tests\TestSecurityManager'
+        ]);
+    }
+    public function testGetCurrentSchoolyear() : void
+    {   
+        $repository = new SchoolyearsRepository(Schoolyears::class);
+
+        $model = $repository->getCurrentSchoolyear();
+        
+        $this->assertInstanceOf(Schoolyears::class, $model);
+    }
+}
