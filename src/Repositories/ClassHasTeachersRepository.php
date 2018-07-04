6d06929fb47bb5023c5b213c0aaef65f4925e86a
Tobias Franek <tobias.franek@gmail.com>
2018-07-04 13:42:24 +0200

    fixed Repo

--- src/Repositories/ClassHasTeachersRepository.php
+++ src/Repositories/ClassHasTeachersRepository.php
@@ -52,7 +52,7 @@ class ClassHasTeachersRepository extends Repository {
                 $class['teachers'] = [];
                 $classesHaveTeachers[] = new $this->model($class);
             }
-            $schoolyear = $query->get('Schoolyear')->getCurrentSchoolyear();
+            $schoolyear = $query->get('Schoolyears')->getCurrentSchoolyear();
             foreach ($classesHaveTeachers as $key => $value) {
                 $periods = $query->get('Period')->findAll([], null, $value->getId(), 1, date_format($schoolyear->getStartDate(), 'Ymd'), date_format($schoolyear->getEndDate(), 'Ymd'));
                 $tempTeachers = [];
