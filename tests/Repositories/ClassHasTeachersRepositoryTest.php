3175b2e413fe0258c6a16e273587bf8209845625
Tobias Franek <tobias.franek@gmail.com>
2018-07-04 13:42:48 +0200

    #19 added RepositoryTest and ClassHasTeachersRepositoryTest

---
+++ tests/Repositories/ClassHasTeachersRepositoryTest.php
@@ -0,0 +1,58 @@
+<?php
+declare(strict_types=1);
+
+namespace Webuntis\Tests\Repositories;
+
+use PHPUnit\Framework\TestCase;
+use Webuntis\Repositories\ClassHasTeachersRepository;
+use Webuntis\Configuration\WebuntisConfiguration;
+use Webuntis\Models\Classes;
+use Webuntis\Models\SchoolYears;
+use Webuntis\Models\Period;
+use Webuntis\Models\Teachers;
+use Webuntis\Models\ClassHasTeachers;
+use Webuntis\Models\Rooms;
+use Webuntis\Models\Subjects;
+use Webuntis\Handler\ExecutionHandler;
+
+final class ClassHasTeachersRepositoryTest extends TestCase
+{
+    public function setUp() : void 
+    {
+        $config = new WebuntisConfiguration([ 
+            'default' => [
+                   //f.e. thalia, cissa etc.
+                    'server' => 'yourserver',
+                    'school' => 'yourschool',
+                    'username' => 'yourusername',
+                    'password' => 'yourpassword'
+                ],
+            'admin' => [
+                   //f.e. thalia, cissa etc.
+                    'server' => 'yourserver',
+                    'school' => 'yourschool',
+                    'username' => 'youradminusername',
+                    'password' => 'youradminpassword'
+            ],
+            'security_manager' => 'Webuntis\Tests\TestSecurityManager'
+        ]);
+    }
+
+    public function testFindAll() : void
+    {   
+        $repository = new ClassHasTeachersRepository(ClassHasTeachers::class);
+        $data = $repository->getInstance()->getClient()->getData();
+        $models = $repository->findAll();
+        $this->assertEquals(4, count($models));
+        $this->assertEquals('test', $models[0]->getName());
+        $this->assertEquals('sie', $models[1]->getName());
+        $this->assertEquals('valid', $models[2]->getName());
+        $this->assertEquals('thisisastring', $models[3]->getName());
+
+        $this->assertArrayHasKey('teachers', $models[0]->serialize());
+        $this->assertArrayHasKey('teachers', $models[1]->serialize());
+        $this->assertArrayHasKey('teachers', $models[2]->serialize());
+        $this->assertArrayHasKey('teachers', $models[3]->serialize());
+
+    }
+}
--- tests/Repositories/RepositoryTest.php
+++ tests/Repositories/RepositoryTest.php
@@ -1,126 +1,55 @@
 <?php
 declare(strict_types=1);
 
-namespace Webuntis\Tests\Query;
+namespace Webuntis\Tests\Repositories;
 
 use PHPUnit\Framework\TestCase;
 use Webuntis\Repositories\Repository;
 use Webuntis\Models\Classes;
 use Webuntis\Models\SchoolYears;
 use Webuntis\Handler\ExecutionHandler;
+use Webuntis\Configuration\WebuntisConfiguration;
 
 final class RepositoryTest extends TestCase
 {
-    private $classesModels = [];
-
-    private $executionHandler;
-
-    protected function setUp()
+    public function setUp() : void 
     {
-        $this->executionHandler = $this->getMockBuilder(ExecutionHandler::class)
-                                       ->getMock();
-        $dataClasses = [
-            [
-                'id' => 1,
-                'name' => 'test',
-                'longName' => 'teststring'
+        $config = new WebuntisConfiguration([ 
+            'default' => [
+                   //f.e. thalia, cissa etc.
+                    'server' => 'yourserver',
+                    'school' => 'yourschool',
+                    'username' => 'yourusername',
+                    'password' => 'yourpassword'
+                ],
+            'admin' => [
+                   //f.e. thalia, cissa etc.
+                    'server' => 'yourserver',
+                    'school' => 'yourschool',
+                    'username' => 'youradminusername',
+                    'password' => 'youradminpassword'
             ],
-            [
-                'id' => 2,
-                'name' => 'sie',
-                'longName' => 'nice'
-            ],
-            [
-                'id' => 3,
-                'name' => 'valid',
-                'longName' => 'one'
-            ],
-            [
-                'id' => 4,
-                'name' => 'thisisastring',
-                'longName' => 'thisisanevenlongerstring'
-            ]
-        ];
-
-        $dataYears = [
-            [
-                'id' => 2,
-                'name' => '2013/2014',
-                'startDate' => '2013-09-09T00:00:00+0200',
-                'endDate' => '2014-07-06T00:00:00+0200'
-            ],
-            [
-                'id' => 3,
-                'name' => '2014/2015',
-                'startDate' => '2014-09-09T00:00:00+0200',
-                'endDate' => '2015-07-06T00:00:00+0200'
-            ],
-            [
-                'id' => 4,
-                'name' => '2015/2016',
-                'startDate' => '2015-09-09T00:00:00+0200',
-                'endDate' => '2016-07-06T00:00:00+0200'
-            ],
-            [
-                'id' => 5,
-                'name' => '2016/2017',
-                'startDate' => '2016-09-09T00:00:00+0200',
-                'endDate' => '2017-07-06T00:00:00+0200'
-            ]
-            ];
-
-        $this->classesModels = [];
-
-        foreach($dataClasses as $modeldataClasses) {
-            $this->classesModels[] = new Classes($modeldataClasses);
-        }
-
-        $this->yearsModels = [];
-
-        foreach($dataYears as $modeldataYears) {
-            $this->yearsModels[] = new Schoolyears($modeldataYears);
-        }
-
-        $this->executionHandler->expects($this->any())
-                               ->method('execute')
-                               ->will($this->returnCallback([$this, 'executeCallback']));
+            'security_manager' => 'Webuntis\Tests\TestSecurityManager'
+        ]);
     }
-
     public function testFindAll() : void
     {   
-        $repository = new Repository(Classes::class, $this->executionHandler);
-        $this->assertEquals($this->classesModels, $repository->findAll());
+        $repository = new Repository(Classes::class);
+        $this->assertEquals(4, count($repository->findAll()));
 
-        $this->assertEquals(array_reverse($this->classesModels), $repository->findAll(['id' => 'DESC']));
+        $this->assertEquals(4, $repository->findAll(['id' => 'DESC'])[0]->getId());
         $this->assertEquals(2, count($repository->findAll([], 2)));
     }
 
     public function testFindBy() : void 
     {
-        $repository = new Repository(Classes::class, $this->executionHandler);
+        $repository = new Repository(Classes::class);
 
-        $expected = [
-            $this->classesModels[2]
-        ];
-        $this->assertEquals($expected, $repository->findBy(['name' => '%d%']));
-        $this->assertEquals($expected, $repository->findBy(['name' => 'valid']));
+        $this->assertEquals('valid', $repository->findBy(['name' => '%d%'])[0]->getName());
+        $this->assertEquals('valid', $repository->findBy(['name' => 'valid'])[0]->getName());
 
-        $repository = new Repository(Schoolyears::class, $this->executionHandler);
+        $repository = new Repository(Schoolyears::class);
 
-        $expected = [
-            $this->yearsModels[0],
-            $this->yearsModels[1],
-            $this->yearsModels[2]
-        ];
-
-        $this->assertEquals($expected, $repository->findBy(['startDate' => '>2013-01-01', 'endDate' => '<2016-12-31']));
-    }
-
-    public function executeCallback($repo) {
-        if($repo->getModel()::METHOD == 'getKlassen') {
-            return $this->classesModels;
-        } else if($repo->getModel()::METHOD == 'getSchoolyears') {
-            return $this->yearsModels;
-        }
+        $this->assertEquals(3, count($repository->findBy(['startDate' => '>2013-01-01', 'endDate' => '<2016-12-31'])));
     }
 }
