06a5d6dfb7c07c732150c5033e5d25807e822ec3
Tobias Franek <tobias.franek@gmail.com>
2018-07-04 20:05:15 +0200

    #19 added UserRepositoryTest

---
+++ tests/Repositories/UserRepositoryTest.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+
+namespace Webuntis\Tests\Repositories;
+
+use PHPUnit\Framework\TestCase;
+use Webuntis\Repositories\UserRepository;
+use Webuntis\Models\Teachers;
+use Webuntis\Handler\ExecutionHandler;
+use Webuntis\Configuration\WebuntisConfiguration;
+
+final class UserRepositoryTest extends TestCase
+{
+    public function setUp() : void 
+    {
+        $config = new WebuntisConfiguration([ 
+            'default' => [
+                   //f.e. thalia, cissa etc.
+                    'server' => 'yourserver',
+                    'school' => 'yourschool',
+                    'username' => 'yourusername',
+                    'password' => 'yourpassword'
+                ],
+            'admin' => [
+                   //f.e. thalia, cissa etc.
+                    'server' => 'yourserver',
+                    'school' => 'yourschool',
+                    'username' => 'youradminusername',
+                    'password' => 'youradminpassword'
+            ],
+            'security_manager' => 'Webuntis\Tests\TestSecurityManager'
+        ]);
+    }
+
+    public function testGetCurrentUser() : void
+    {   
+        $repo = new UserRepository();
+
+        $this->assertInstanceOf(Teachers::class, $repo->getCurrentUser());
+    }
+
+    public function testGetCurrentUserType() : void
+    {   
+        $repo = new UserRepository();
+
+        $this->assertEquals(2, $repo->getCurrentUserType());
+    }
+}
