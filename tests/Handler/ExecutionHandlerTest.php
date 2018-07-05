310a109f838a69bf3094ef8facefd9bcd78068f8
Tobias Franek <tobias.franek@gmail.com>
2018-07-05 11:24:35 +0200

    #19 added ExecutionHandlerTest

---
+++ tests/Handler/ExecutionHandlerTest.php
@@ -0,0 +1,68 @@
+<?php
+declare(strict_types=1);
+
+namespace Webuntis\Tests\Handler;
+
+use PHPUnit\Framework\TestCase;
+use Webuntis\Repositories\Repository;
+use Webuntis\Models\Classes;
+use Webuntis\Configuration\WebuntisConfiguration;
+use Webuntis\Handler\ExecutionHandler;
+
+final class ExecutionHandlerTest extends TestCase
+{
+    public function setUp() : void 
+    {
+        $config = new WebuntisConfiguration([ 
+            'default' => [
+                   //f.e. thalia, cissa etc.
+                    'server' => 'yourserver',
+                    'school' => 'yourschool',
+                    'username' => 'yourusername',
+                    'password' => 'yourpassword'
+                ],
+            'admin' => [
+                   //f.e. thalia, cissa etc.
+                    'server' => 'yourserver',
+                    'school' => 'yourschool',
+                    'username' => 'youradminusername',
+                    'password' => 'youradminpassword'
+            ],
+            'security_manager' => 'Webuntis\Tests\TestSecurityManager'
+        ]);
+    }
+
+    public function testCreate() : void
+    {   
+        $executionHandler = new ExecutionHandler();
+
+        $repo = new Repository(Classes::class);
+
+        $data = $executionHandler->execute($repo, []);
+
+        $expected = [
+            [
+                'id' => 1,
+                'name' => 'test',
+                'fullName' => 'teststring'
+            ],
+            [
+                'id' => 2,
+                'name' => 'sie',
+                'fullName' => 'nice'
+            ],
+            [
+                'id' => 3,
+                'name' => 'valid',
+                'fullName' => 'one'
+            ],
+            [
+                'id' => 4,
+                'name' => 'thisisastring',
+                'fullName' => 'thisisanevenlongerstring'
+            ]
+        ];
+
+        $this->assertEquals($expected, \Webuntis\Serializer\Serializer::serialize($data));
+    }
+}
