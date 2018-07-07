4988ec88118e36696a7e2208a54ca4f3e998ee82
Tobias Franek <tobias.franek@gmail.com>
2018-07-07 12:35:07 +0200

    #11 added ClassRegEvents Model and repo

---
+++ src/Models/ClassRegEvents.php
@@ -0,0 +1,193 @@
+<?php
+declare(strict_types=1);
+
+namespace Webuntis\Models;
+
+use Webuntis\Models\AbstractModel;
+use Webuntis\Models\Interfaces\CachableModelInterface;
+use Webuntis\Models\Interfaces\AdministrativeModelInterface;
+use DateTime;
+use Webuntis\Exceptions\ModelException;
+
+/**
+ * ClassRegEvents Model
+ * @author Tobias Franek <tobias.franek@gmail.com>
+ * @license MIT
+ */
+class ClassRegEvents extends AbstractModel implements CachableModelInterface, AdministrativeModelInterface
+{
+    const CACHE_LIFE_TIME = 86400;
+    const METHOD = "getClassregEvents";
+
+    /**
+     * @var AbstractModel
+     */
+    private $student;
+
+    /**
+     * @var DateTime
+     */
+    private $date;
+
+    /**
+     * @var string
+     */
+    private $subject;
+
+    /**
+     * @var string
+     */
+    private $reason;
+
+    /**
+     * @var string
+     */
+    private $text;
+
+    /**
+     * Getter for student
+     *
+     * @return Webuntis\Models\AbstractModel
+     */
+    public function getStudent() : AbstractModel
+    {
+        return $this->student;
+    }
+
+    /**
+     * Setter for student
+     *
+     * @param AbstractModel $student
+     * @return ClassRegEvents
+     */
+    public function setStudent(AbstractModel $student) : self
+    {
+        $this->student = $student;
+
+        return $this;
+    }
+
+    /**
+     * Getter for date
+     *
+     * @return DateTime
+     */
+    public function getDate() : DateTime
+    {
+        return $this->date;
+    }
+
+    /**
+     * Setter for date
+     *
+     * @param DateTime $date
+     * @return ClassRegEvents
+     */
+    public function setDate(DateTime $date) : self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Getter for subject
+     *
+     * @return string
+     */
+    public function getSubject() : string
+    {
+        return $this->subject;
+    }
+
+    /**
+     * Setter for subject
+     *
+     * @param string $subject
+     * @return ClassRegEvents
+     */
+    public function setSubject(string $subject) : self
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    /**
+     * Getter for reason
+     *
+     * @return string
+     */
+    public function getReason() : string
+    {
+        return $this->reason;
+    }
+
+    /**
+     * Setter for reason
+     *
+     * @param string $reason
+     * @return ClassRegEvents
+     */
+    public function setReason(string $reason) : self
+    {
+        $this->reason = $reason;
+
+        return $this;
+    }
+
+    /**
+     * Getter for text
+     *
+     * @return string
+     */
+    public function getText() : string
+    {
+        return $this->text;
+    }
+
+    /**
+     * Setter for text
+     *
+     * @param string $text
+     * @return ClassRegEvents
+     */
+    public function setText(string $text) : self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * sets an given field
+     *
+     * @param mixed $field
+     * @param mixed $value
+     * @return ClassRegEvents
+     */
+    public function set($field, $value) : self
+    {
+        $this->$field = $value;
+
+        return self;
+    }
+
+    /**
+     * return the children by given key
+     *
+     * @param string $key
+     *
+     * @return AbstractModel[]
+     * @throws ModelException
+     */
+    public function get($key) : array
+    {
+        switch ($key) {
+            case 'student':
+                return [$this->student];
+            default:
+                throw new ModelException("array of objects $key doesn't exist");
+        }
+    }
+}
---
+++ src/Repositories/ClassRegEventsRepository.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Webuntis\Repositories;
+
+use Webuntis\Repositories\Repository;
+
+/**
+ * ClassRegEventsRepository
+ * @author Tobias Franek <tobias.franek@gmail.com>
+ * @license MIT
+ */
+class ClassRegEventsRepository extends Repository
+{
+}
