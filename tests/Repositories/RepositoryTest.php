4b8bbf6fb6456d960eaf2c2ac8d2cba9b74da5b1
Tobias Franek <tobiasfranek@localhost.localdomain>
2018-08-31 16:57:19 +0200

    added additonal Tests to check the ignore child property better

--- tests/Repositories/RepositoryTest.php
+++ tests/Repositories/RepositoryTest.php
@@ -7,7 +7,10 @@ use PHPUnit\Framework\TestCase;
 use Webuntis\Repositories\Repository;
 use Webuntis\Models\Classes;
 use Webuntis\Models\SchoolYears;
+use Webuntis\Models\Period;
+use Webuntis\Models\Substitutions;
 use Webuntis\Handler\ExecutionHandler;
+use Webuntis\Exceptions\RepositoryException;
 use Webuntis\Configuration\WebuntisConfiguration;
 
 /**
@@ -58,5 +61,38 @@ final class RepositoryTest extends TestCase
         $repository = new Repository(Schoolyears::class);
 
         $this->assertEquals(3, count($repository->findBy(['startDate' => '>2013-01-01', 'endDate' => '<2016-12-31'])));
+
+        WebuntisConfiguration::setConfig([ 
+            'default' => [
+                   //f.e. thalia, cissa etc.
+                    'server' => 'yourserver',
+                    'school' => 'yourschool',
+                    'username' => 'yourusername',
+                    'password' => 'yourpassword',
+                    'ignore_children' => true
+                ],
+            'admin' => [
+                   //f.e. thalia, cissa etc.
+                    'server' => 'yourserver',
+                    'school' => 'yourschool',
+                    'username' => 'youradminusername',
+                    'password' => 'youradminpassword'
+            ],
+            'security_manager' => 'Webuntis\Tests\Util\TestSecurityManager'
+        ]);
+        $repository = new Repository(Substitutions::class);
+
+        $this->assertEquals(2, count($repository->findAll()));
+
+        $repository = new Repository(Period::class);
+
+        $this->assertInternalType('array', $repository->findAll()[0]->getClasses());
+        $this->assertInternalType('array', $repository->findAll()[0]->getTeachers());
+        $this->assertInternalType('array', $repository->findAll()[0]->getSubjects());
+        $this->assertInternalType('array', $repository->findAll()[0]->getRooms());
+
+        $this->expectException(RepositoryException::class);
+        $this->assertInternalType('array', $repository->findBy(['classes.name' => 'test']));
+
     }
 }
