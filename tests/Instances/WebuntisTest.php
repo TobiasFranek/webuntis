2b152a4c6176dbf93d2bc904caaf13c43bc693ae
Tobias Franek <tobiasfranek@localhost.localdomain>
2018-08-31 17:22:45 +0200

    added some tests to ensure the working of user_type

--- tests/Instances/WebuntisTest.php
+++ tests/Instances/WebuntisTest.php
@@ -5,6 +5,7 @@ namespace Webuntis\Tests\Instances;
 
 use PHPUnit\Framework\TestCase;
 use Webuntis\Webuntis;
+use Webuntis\Models\Students;
 use Webuntis\Configuration\WebuntisConfiguration;
 
 /**
@@ -60,5 +61,19 @@ final class WebuntisTest extends TestCase
         $this->assertEquals('yourserver.yourschool.com', $instance->getPath());
         $this->assertEquals('admin', $instance->getContext());
         $this->assertTrue(method_exists($instance->getClient(), 'call'));
+
+        $config = [
+            //f.e. thalia, cissa etc.
+             'server' => 'yourserver',
+             'school' => 'yourschool',
+             'username' => 'youradminusername',
+             'password' => 'youradminpassword',
+             'path_scheme' => '{server}.{school}.com',
+             'user_type' => 'Students'
+        ];
+        $instance = new Webuntis($config, 'admin');
+        $this->assertEquals(2, $instance->getCurrentUserType());
+        $this->assertInstanceOf(Students::class, $instance->getCurrentUser());
+
     }
 }
