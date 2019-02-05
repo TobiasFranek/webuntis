2d803bec2f6f89a8c9f78a3bfdc2a875509dce4e
Tobias Franek <tobias.franek@gmail.com>
2019-02-05 14:12:16 +0100

    fixed bug with overflowing numbers

--- src/Types/TypeHandler.php
+++ src/Types/TypeHandler.php
@@ -74,7 +74,7 @@ class TypeHandler {
             if (isset($implements[TypeInterface::class])) {
                 if(($value['type'] == 'model' || $value['type'] == 'modelCollection') && (isset($instanceConfig['ignore_children']) && $instanceConfig['ignore_children'])) {
                     if($value['type'] == 'model') {
-                        if(is_numeric($data[$value['api']['name']])) {
+                        if(is_numeric($data[$value['api']['name']]) && $data[$value['api']['name']] < PHP_INT_MAX) {
                             self::$types['int']::execute($model, $data, [$key => $value]);
                         } else {
                             self::$types['string']::execute($model, $data, [$key => $value]);
